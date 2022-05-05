d27b92e4192d0afd2f7e5cdfc670cc49143a75cd
saivamsi09 <saivamsi.b2773@gmail.com>
2022-05-05 11:52:43 +0530

    initial commit

---
+++ CurdOperations/src/test/java/com/example/CurdOperations/CurdOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurdOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
